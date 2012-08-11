c62f931b8b4f82f0c76cc30862c232b02175bee7
Al Wold <alwold@gmail.com>
2012-08-11 11:43:13 -0700

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluebanana::Application.config.secret_token = '22fd1b2f6628c864f3ec2b30936ffe476cf67278f7cd65724cace9ce4ebd8a04aeba9cc5414b6d41a358bc839c76dc66fc9d8d8a3766377041dfea006ad455a6'
