e53e8a8e41904d6d8c91b1c2f0ba5a7ce36bf36e
Al Wold <alwold@gmail.com>
2012-08-11 20:33:46 -0700

    a comment

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -5,6 +5,7 @@ class AttendeesController < ApplicationController
     logger.debug event.eventbrite_id
     attendees = EventBrite.get("/event_list_attendees", :query => { :id => event.eventbrite_id, :app_key => "4BVWLKSTKBR2VG2FSL" })
 
+    # TODO clean up data and indicate if the user is checked in
     respond_to do |format|
       format.json { render :json => attendees }
     end
