5b4af8b0e66e416aa24693bc593b0ce62e09dfe5
Al Wold <alwold@gmail.com>
2012-08-12 12:36:42 -0700

    add mapping to checksin

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ActiveRecord::Base
   has_many :meetings
+  has_many :checkins
   
   attr_accessible :eventbrite_id, :title, :description, :start, :end, :hashtag, :latitude, :longitude
 end
