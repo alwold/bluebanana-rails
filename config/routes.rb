b8737a9b36f3e33996b803c179b593d17659936b
Al Wold <alwold@gmail.com>
2012-08-12 13:00:21 -0700

    add a home controller with a bit of content

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Bluebanana::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'events#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
